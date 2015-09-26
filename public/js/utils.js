b0578426928089e2f9c640c8edfc1a7a29b0fba0
apple <apple@lucys-mac.local>
2015-09-25 20:10:27 -0400

    ad functions showNotice and hideNotice

--- public/js/utils.js
+++ public/js/utils.js
@@ -48,5 +48,16 @@ splat.utils = {
 	// When all deferred template-loads have completed,
 	// invoke callback function.
         $.when.apply(null, deferreds).done(callback);
+    },
+
+    showNotice: function(text, alert-type) {
+        $('.alert').removeClass("alert-error alert-warning alert-success alert-info");
+        $('.alert').addClass(alert-type);
+        $('.alert').html('<strong> Note! </strong> ' + text);
+        $('.alert').show();
+    },
+
+    hideNotice: function() {
+        $('.alert').stop().fadeOut();
     }
 };
