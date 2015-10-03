b67ddf82dba7e587f0b80cc3b0cfbc5ba2862464
Yadi Xing <yadi.xing@mail.utoronto.ca>
2015-10-03 15:27:42 -0400

    add modify functionality

--- public/js/views/details.js
+++ public/js/views/details.js
@@ -20,6 +20,38 @@ splat.Details = Backbone.View.extend({
 	},
 
 	addHandler: function() {
+		if(this.model.id){
+			console.log(this.model.id);
+			var model = this.collection.get(this.model.id);
+			model.set({
+				title: $("#title").val(),
+				released: $("#released").val(),
+				director: $("#director").val(),
+				rating: $("#rating").val(),
+				starring: $("#starring").val(),
+				duration: $("#duration").val(),
+				genre: $("#genre").val(),
+				synopsis: $("#synopsis").val(),
+				trailer: $("#trailer").val(),
+				},
+				{
+				wait: true,  // don't destroy client model until server responds
+    			success: function(response) {   		
+					// later, we'll navigate to the browse view upon success
+        			splat.app.navigate('#movies/' + response.id, {replace:true, trigger:true});
+					// notification panel, defined in section 2.6
+        			splat.utils.showNotice('Success', "Movie updated", 'alert-success')
+        			splat.utils.hideNotice()
+    			},
+    			error: function(response) {
+    				// display the error response from the server
+        			splat.utils.requestFailed(response);
+        			splat.utils.showNotice('Fail', "Movie was not sucessfully updated", 'alert-error')
+        			splat.utils.hideNotice()        			
+    			},
+    		});
+		}
+		else{
 		var newModel = this.collection.create({
 				title: $("#title").val(),
 				released: $("#released").val(),
@@ -46,6 +78,7 @@ splat.Details = Backbone.View.extend({
         		splat.utils.hideNotice()        			
     		},
 		});
+		}
 	},
 	
 	deleteHandler: function() {
